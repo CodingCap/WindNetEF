f5274498a62423234a93b8dfc4b8e315d2476620
Albu Valentin <albuvalentin23@yahoo.com>
2020-01-21 22:34:15 +0200

    added DbSets

--- WindNetEF/WindNetEF/WindNetDbContext.cs
+++ WindNetEF/WindNetEF/WindNetDbContext.cs
@@ -4,12 +4,16 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindNetEF.Models;
 
 namespace WindNetEF
 {
     public class WindNetDbContext : DbContext
     {
-        //public DbSet<Model> Models { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Address> Addresses { get; set; }
 
         public WindNetDbContext()
             : base("name=WindNetConnString")
